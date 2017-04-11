f070ef5bb85d835603e3580d3747d372fa391846
nazmul56 <nazmul.cste07@gmail.com>
2017-04-11 12:53:02 +0600

    Third Batch

---
+++ fact_father.pl
@@ -0,0 +1,8 @@
+father(priam, hector).
+father(laertes,ulysses).
+father(atreus,menelaus).
+father(menelaus, hermione).
+father(ulysses, telemachus).
+mother(hecuba, hector).
+mother(penelope,telemachus).
+mother(helen, hermione).
